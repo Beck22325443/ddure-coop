937c04c57d44b78b27edd85bbd081c9dadad2c27
Beck22325443 <hansin94_@naver.com>
2026-07-03 12:43:22 -0700

    Add Open Graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
-  title: "뚜레 협동작전",
-  description: "뚜레 협동작전",
-};
+  title: "DDURE COOP",
+  description: "니케 협동작전 모집 사이트",
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
-}
+  openGraph: {
+    title: "DDURE COOP",
+    description: "니케 협동작전 모집 사이트",
+    images: ["/og.png"],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "DDURE COOP",
+    description: "니케 협동작전 모집 사이트",
+    images: ["/og.png"],
+  },
+};
