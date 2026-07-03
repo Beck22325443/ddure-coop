8e42cc607707e956c73939f0a84f72d20e154dd7
Beck22325443 <hansin94_@naver.com>
2026-07-03 12:50:55 -0700

    Fix layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,31 @@
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "DDURE COOP",
+  title: "뚜레 협동작전",
   description: "니케 협동작전 모집 사이트",
-
   openGraph: {
-    title: "DDURE COOP",
+    title: "뚜레 협동작전",
     description: "니케 협동작전 모집 사이트",
     images: ["/og.png"],
     type: "website",
   },
-
   twitter: {
     card: "summary_large_image",
-    title: "DDURE COOP",
+    title: "뚜레 협동작전",
     description: "니케 협동작전 모집 사이트",
     images: ["/og.png"],
   },
 };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
