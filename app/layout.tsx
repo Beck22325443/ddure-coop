5acd142778cbc0390844fac27630bd5293c7b130
Beck22325443 <hansin94_@naver.com>
2026-07-03 13:08:04 -0700

    Fix layout root export

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,27 +2,27 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "뚜레 협동작전",
-  description: "니케 협동작전 모집 사이트",
+  title: "뚜레의 오븐 협동작전",
+  description: "뚜레 협동작전 사이트",
   openGraph: {
-    title: "뚜레 협동작전",
-    description: "니케 협동작전 모집 사이트",
+    title: "뚜레의 오븐 협동작전",
+    description: "뚜레 협동작전 사이트",
     images: ["/toast.png"],
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "뚜레 협동작전",
-    description: "니케 협동작전 모집 사이트",
+    title: "뚜레의 오븐 협동작전",
+    description: "뚜레 협동작전 사이트",
     images: ["/toast.png"],
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ko">
       <body>{children}</body>
